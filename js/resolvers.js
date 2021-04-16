8a442e8f2b892fccab756a4cad255be67cb73732
veeso <christian.visintin1997@gmail.com>
2021-04-16 10:26:01 +0200

    Video fallback

--- js/resolvers.js
+++ js/resolvers.js
@@ -7,27 +7,43 @@ function resolveAge() {
   const ageDate = new Date(diff);
   const age = Math.abs(ageDate.getUTCFullYear() - 1970);
   // Replace all tags
-  $(".resolve-age").each(function () {
+  $("[resolve-age]").each(function () {
     $(this).text(age);
   });
 }
 
+/**
+ * @description resolve copyright year
+ */
+
 function resolveCopyright() {
   const year = new Date().getFullYear();
-  $(".resolve-copyright").each(function () {
+  $("[resolve-copyright]").each(function () {
     $(this).text(year);
   });
 }
 
 /**
- * @description call all resolvers
+ * @description resolve video fallback source in case fails. Uses an image instead
  */
-function resolve() {
-  resolveAge();
-  resolveCopyright();
+function resolveVideoFallback() {
+  $("[resolve-video-fallback]").each(function () {
+    const fallback = $(this).attr("resolve-video-fallback");
+    // Add listener
+    console.log(fallback, this);
+    $(this).on("error", function () {
+      const image = document.createElement("img");
+      image.src = fallback;
+      image.classList = ["preview"];
+      console.log($(this).parent());
+      $(this).parent().replaceWith(image);
+    });
+  });
 }
 
 // init
 $(function () {
-  resolve();
+  resolveAge();
+  resolveCopyright();
+  resolveVideoFallback();
 });
